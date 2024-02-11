8822f364b0522690788707f2e4fc42ce9da3c8bc
Martin Sigloch <f0i.github@projects.f0i.de>
2024-02-11 02:47:07 +0100

    Implement UserAccount component (#6)
    feat: add and edit proxy identities
    feat: persist selected PIID in localStorage
    feat: prepare zod schema validation
    
    References: IN3-29

--- src/frontend/src/components/UserAccount.vue
+++ src/frontend/src/components/UserAccount.vue
@@ -1,33 +1,162 @@
 <template>
-    <button v-if="!isSignedIn" class="btn btn-slim btn-primary text-white !h-[36px]">
+    <button v-if="!isSignedIn" class="btn btn-slim btn-primary text-white mr-3" @click="login">
         Sign In
     </button>
 
-    <div v-if="isSignedIn"
-        class="dropdown dropdown-end pr-2" :class="[componentName]">
+    <div v-if="isSignedIn" class="dropdown dropdown-end mr-2" :class="[componentName]">
         <div tabindex="0" role="button" class="flex avatar pl-4 pr-0">
             <div class="w-6 lg:w-10 rounded-full">
                 <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
             </div>
         </div>
         <ul tabindex="0" class="dropdown-content z-[1] menu mt-2 p-2 shadow bg-base-100 rounded-box w-52">
-            <li><a>Item 1</a></li>
-            <li><a>Item 2</a></li>
+            <li v-if="account" class="menu-title">ProxyIdentities</li>
+            <li v-for="piid in account?.data.piids">
+                <div v-if="editPiid?.piid !== piid.piid" class="flex">
+                    <button v-if="!isActive(piid, selectedPiid)" @click="selectPiid(piid)" class="flex-grow text-left">
+                        {{ piid.name }}
+                    </button>
+                    <a v-if="isActive(piid, selectedPiid)" :href="'/proxy-identities/' + piid.piid" class="flex-grow">{{
+                        piid.name }}</a>
+                    <!-- TODO: replace emoji with font icon -->
+                    <span v-if="isActive(piid, selectedPiid)">👈</span>
+                    <a @click="editPiid = piid">✏️ </a>
+                    <a :href="'/proxy-identities/' + piid.piid">📋</a> <!-- TODO: implement copy to clipboard -->
+                </div>
+                <!-- edit mode -->
+                <div v-if="editPiid?.piid === piid.piid" class="flex menu-title">
+                    <!-- TODO: autofocus when edit mode is activated -->
+                    <input @input="event => setPiidName({ ...piid, name: ((event.target as any)?.value ?? piid.name) })"
+                        type="text" :value="piid.name" class="flex-grow w-10 input mr-2 p-1" />
+                    <button @click="saveAccount(); false" class="btn mr-[-10px]">✔️ </button>
+                </div>
+            </li>
+            <li v-if="account" @click="setPiidName(createPiid())">
+                <button class="btn btn-slim m-2">➕</button>
+            </li>
+            <li v-if="!account" class="menu-title text-center">
+                Loading...
+            </li>
+            <li>
+                <hr />
+            </li>
+            <li>
+                <button @click="logout" class="text-center">Sign out</button>
+            </li>
         </ul>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from 'vue';
+import { authSubscribe, getDoc, init, login, logout, setDoc } from '../junoHelper';
+import type { Doc, User } from '@junobuild/core';
+import type { IUserAccount } from '../contracts/IUserAccount'
+import type { INamedProxyIdentity, IProxyIdentity } from '../contracts/IProxyIdentity'
+import { urlAlphabet, customAlphabet } from 'nanoid'
+const nanoid = customAlphabet(urlAlphabet, 32);
+import { useStore } from '@nanostores/vue'
+import { deepMap } from 'nanostores';
+
+
+
 export const componentName = 'UserAccount'
 export default defineComponent({
     name: componentName,
-})
+});
 </script>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
-const isSignedIn = ref(false)
-// code
+import { ref } from 'vue';
+const isSignedIn = ref(false);
+const user = ref<null | User>(null);
+const account = ref<Doc<IUserAccount> | null>(null);
+const editPiid = ref<INamedProxyIdentity | null>(null);
+const selectedPiid = ref<IProxyIdentity | null>(null);
+
+init();
+
+{
+    const piid = localStorage.getItem("IN3-UserAccount-PIID");
+    if (piid) selectedPiid.value = { piid };
+}
+
+const selectPiid = (piid: INamedProxyIdentity) => {
+    selectedPiid.value = piid;
+    window.localStorage.setItem("IN3-UserAccount-PIID", piid.piid);
+};
+
+const loadAccount = async (user: User) => {
+    // TODO: handle error from getDoc and setDoc
+    let doc = await getDoc<IUserAccount>({
+        collection: 'user_account',
+        key: user.key,
+    })
+
+    if (!doc) {
+        const data: Doc<IUserAccount> = {
+
+            key: user.key, // random id
+            data: {
+                piids: [createPiid()], // proxyIdentityIds
+                state: 'active',
+            },
+            owner: user.key,  // IdentityId Principle
+            description: '', // can be empty because every principle sees only his own entries, except controllers
+        }
+        console.log("create new user account", data);
+        doc = await setDoc({
+            collection: 'user_account',
+            doc: data,
+        })
+    }
+
+    console.log("user accound loaded", doc);
+    account.value = doc;
+}
+
+const saveAccount = async () => {
+    let data = account.value;
+    if (!data) throw "saveAccount called but account is not set";
+
+    editPiid.value = null;
+
+    const doc = await setDoc({
+        collection: 'user_account',
+        doc: data,
+    })
+    console.log("user account saved", doc)
+}
+
+// Creates a new ProxyIdentity and activate editing
+const createPiid = (): INamedProxyIdentity => {
+    let name = "P" + nanoid(5);
+    return { name, piid: name + nanoid(32 - 5) };
+}
+
+/// Add or update a PIID in account
+const setPiidName = (piid: { piid: string, name: string }) => {
+    let acc = account.value;
+    if (!acc) throw "setPiidName called but account is not set";
+
+    const index = acc.data.piids.findIndex((p) => p.piid === piid.piid);
+    if (index >= 0) {
+        acc.data.piids[index] = piid;
+    } else {
+        acc.data.piids.push(piid)
+    }
+}
+
+authSubscribe((u) => {
+    isSignedIn.value = u !== null;
+    user.value = u;
+    if (u !== null) {
+        loadAccount(u);
+    }
+})
+
+const isActive = (piid: IProxyIdentity, active: IProxyIdentity | null): boolean => {
+    return active?.piid === piid.piid;
+}
 
 </script>
